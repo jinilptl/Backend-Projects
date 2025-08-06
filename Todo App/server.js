10f5c43a1a9348f097f8dac33eee4c5df7307348
jinilptl <jrpatel1747@gmail.com>
2025-08-06 19:06:46 +0530

    folder structure create is done

--- Todo App/server.js
+++ Todo App/server.js
@@ -1,1 +1,14 @@
-console.log('express started');
+const express=require('express');
+
+const app=express();
+
+const PORT=4000;
+
+app.get('/',(req,res)=>{
+  res.send('<p>welcome to the home page of Todo list</p>')
+})
+app.listen(PORT,()=>{
+  console.log(`server started on port number ${PORT}`);
+  
+})
+
